71ecf5d9ea3d07e3978674c9a53bc305f33d369f
Alkhanm <Joas7777777@hotmail.com>
2019-06-30 14:43:36 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -20,6 +20,9 @@ public abstract class ChessPiece extends Piece {
 		return p != null && p.getColor() != color; 
 	}
 	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
 	
 public Color getColor() {
 		return color;
